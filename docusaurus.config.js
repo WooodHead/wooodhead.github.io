b14aa12fe660c7e0f9505388d803f8a1c7a3dcb8
wushaowei.wsw <wushaowei.wsw@alibaba-inc.com>
2021-05-04 11:16:08 +0800

    save

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://wooodhead.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wooodhead', // Usually your GitHub org/user name.
+  projectName: 'wooodhead.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
